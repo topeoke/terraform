fa692fc2e72c5ba39e60a0a2a68ea61ef9552e9b
= <=>
2019-08-03 18:32:13 +0100

    creating the  tf file for spinnning up a new EC2 instance

---
+++ project1/createvpc.tf
@@ -0,0 +1,19 @@
+
+provide "aws" {
+  profile = "default"
+  region = "eu-west-2"
+}
+
+resource "aws_instance" "instance1" {
+  ami = "ami-0d8e27447ec2c8410"
+  instance_type = "t2-micro"
+  key_name = "packetlaneLondonReg-KeyPairs"
+
+  tags = {
+    Name = 'instance1'
+  }
+}
+
+output "Instance ID" {
+  value = "${aws_instance.instance1.id}"
+}
