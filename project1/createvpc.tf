66f7d847868ec58f275a9203dbb461845ec65d2e
= <=>
2019-08-03 18:48:32 +0100

    updated file

--- project1/createvpc.tf
+++ project1/createvpc.tf
@@ -1,19 +1,17 @@
-
 provide "aws" {
   profile = "default"
   region = "eu-west-2"
 }
-
 resource "aws_instance" "instance1" {
   ami = "ami-0d8e27447ec2c8410"
   instance_type = "t2-micro"
   key_name = "packetlaneLondonReg-KeyPairs"
 
   tags = {
-    Name = 'instance1'
+    Name = "instance1"
   }
 }
 
-output "Instance ID" {
+output "InstanceID" {
   value = "${aws_instance.instance1.id}"
 }
